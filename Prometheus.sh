9403326152309d79c969e2aba4c3b7f2f95d346e
Harsha8464 <124258848+Harsha8464@users.noreply.github.com>
2023-08-15 19:02:52 +0530

    Create Prometheus.sh

---
+++ Prometheus.sh
@@ -0,0 +1,15 @@
+#Add helm repo
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+
+#Update helm repo
+helm repo update
+
+#Install helm
+helm install prometheus prometheus-community/prometheus
+
+#Expose Prometheus Service
+#This is required to access prometheus-server using your browser.
+
+kubectl expose service prometheus-server --type=NodePort --target-port=9090 --name=prometheus-server-ext
+
+minikube service <service name> --url
